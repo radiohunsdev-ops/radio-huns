5d815008ba57f26b259af56bca733d96c7df83a2
Rakesh Bora <rakesh.bora@fineqia.com>
2026-03-24 22:32:15 +0530

    meta tags

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -7,6 +7,14 @@ export const metadata = {
   title: 'Radio Huns',
   description:
     'Radio Huns, the voice of South Asian community in Ottawa since 2001, is the longest running South Asian radio program in North America.',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
